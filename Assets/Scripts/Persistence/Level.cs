eb1b47e6bc3ae65639c98a14031c6b8e6b39c0b0
Csányi Gábor <csanyigabor15@gmail.com>
2023-11-24 00:08:17 +0100

    Added db classes and queries

---
+++ Assets/Scripts/Persistence/Level.cs
@@ -0,0 +1,26 @@
+﻿using Firebase.Firestore;
+
+namespace Assets.Scripts.Persistence
+{
+	public enum LevelTypes
+	{
+		Coloring,
+		Distribution,
+		Lock,
+		TileTrap,
+		Traveling,
+	};
+
+	[FirestoreData]
+	public class Level
+	{
+		[FirestoreDocumentId]
+		public string Id { get; set; }
+
+		[FirestoreProperty]
+		public int Number { get; set; }
+
+		[FirestoreProperty]
+		public LevelTypes Type { get; set; }
+	}
+}
---
+++ Assets/Scripts/Persistence/Path.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Firebase.Firestore;
+
+namespace Assets.Scripts.Persistence
+{
+	public enum Subtopics 
+	{
+		Addition,
+		Subtraction,
+		Multiplication,
+		Division,
+	}
+
+	[FirestoreData]
+	public class Path
+	{
+		[FirestoreDocumentId]
+		public string Id { get; set; }
+
+		[FirestoreProperty]
+		public Subtopics Subtopic { get; set; }
+
+		[FirestoreProperty]
+		public List<Level> Levels { get; set; }
+	}
+}
