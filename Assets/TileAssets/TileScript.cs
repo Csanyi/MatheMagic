40956335dad3a8328e96de3b075e91c86d159422
Júlia Knoch <knoch.juku@gmail.com>
2023-11-28 00:35:35 +0100

    Tile game GUI skeleton
    Basic GUI setup for the Tile game. New scene added for the tile game with backbone GameObjects. First iteration on game logic interface (see in code: TileAssets\TileGameLogicScript).

---
+++ Assets/TileAssets/TileScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TileScript : MonoBehaviour
+{
+    private TextMeshProUGUI myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
